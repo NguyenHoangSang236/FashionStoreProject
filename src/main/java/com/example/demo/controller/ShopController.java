966955ac4a60607b4db00a65823b1ff314361ef4
nguyenhoangsang236 <nguyenhoangsang236@gmail.com>
2022-10-21 22:36:40 +0700

    agdsgs

--- src/main/java/com/example/demo/controller/ShopController.java
+++ src/main/java/com/example/demo/controller/ShopController.java
@@ -15,10 +15,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.example.demo.entity.Product;
+import com.example.demo.respository.Product.ProductRepository;
+
 @Controller
 public class ShopController {
+    @Autowired
+    ProductRepository productRepo;
+  
+    
     @GetMapping("/shopproduct")
-    public String showShop() {
+    public String showShop(Model model) {
+        List<Product> productsList = productRepo.getAllProducts();
+        
+        model.addAttribute("productsList", productsList);
+        
         return "shop";
     }
 }
